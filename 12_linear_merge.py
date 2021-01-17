6a23300cc1576ffa9c00498ad0875195ebe1c020
André Rodrigues <andre.rodriguesneves@gmail.com>
2021-01-17 18:28:54 -0300

    Update 12_linear_merge.py
    exe 12

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -11,7 +11,10 @@ A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 
 def linear_merge(list1, list2):
     # +++ SUA SOLUÇÃO +++
-    return
+    return list(merge(list1, list2))
+
+    
+
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
